072764c8c4a93b57848b1735011d67c156dbcdf6
Vitalik_perfilov <lxperfilov@gmail.com>
2022-02-17 14:01:13 +0300

    Fixed a critical bug

--- IPv4/BloodyAlpha.cs
+++ IPv4/BloodyAlpha.cs
@@ -103,8 +103,7 @@ namespace IPv4
 
         private void label3_Click(object sender, EventArgs e)
         {
-            Form1 newForm = new Form1();
-            newForm.Show();
+
         }
     }
 }
